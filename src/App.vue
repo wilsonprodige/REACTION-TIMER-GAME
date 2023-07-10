14404f8f529bacafbc12e0ff342d02a4f5ac1c5e
wilsonprodige <wilsonzeugang@gmail.com>
2023-07-10 01:26:39 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,71 @@
+<template>
+  <h1>Ninja Reaction Timer</h1>
+  <button class="btn" @click="start()" :disabled="isPlaying">Play</button>
+  <Block v-if="isPlaying" :delay="delay" @end="endGame"/>
+  <Result v-if="showResults" :score="score"/>
+</template>
+<!--learning vue-->
+
+<script>
+import Block from './components/Block.vue'
+import Result from './components/Result.vue'
+
+export default {
+  name: 'App',
+  data(){
+    return {
+      isPlaying: false,
+      delay:null,
+      score:null,
+      showResults: false
+
+    }
+
+  },
+  components: { Block,Result },
+  methods:{
+    start(){
+      this.delay=2000+Math.random() * 5000
+      this.isPlaying=true
+      this.showResults=false
+      
+    },
+    endGame(reactionTime){
+      this.score=reactionTime
+      this.isPlaying=false
+      this.showResults=true
+
+
+
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #444;
+  margin-top: 60px;
+}
+.btn{
+  background: #0faf07;
+  color: white;
+  border: none;
+  padding: 8px 16px;
+  border-radius: 4px;
+  font-size: 16px;
+  letter-spacing: 1px;
+  cursor: pointer;
+  margin:10px;
+
+}
+.btn[disabled]{
+  opacity: 0.2;
+  cursor: not-allowed;
+
+}
+</style>
---
+++ src/components/Result.vue
@@ -0,0 +1,36 @@
+<template>
+  <p>Reaction time: {{ score }} ms</p>
+  <p class="rank">{{ rank }}</p>
+</template>
+
+<script>
+export default {
+    props:['score'],
+    data(){
+        return {
+            rank: null
+        }
+    },
+    mounted(){
+        if(this.score < 250){
+            this.rank="Ninger Fingers"
+        }
+        else if(this.score < 400){
+            this.rank="rapid fingers"
+        }
+        else{
+            this.rank="small pace...."
+        }
+    }
+
+}
+</script>
+
+<style>
+.rank{
+    font-size: 1.4em;
+    color: #0faf07;
+    font-weight: bold;
+}
+
+</style>
